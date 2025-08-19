b2796eb8791ff2b14a37720ad8b6fc97940ae45d
skykanin <3789764+skykanin@users.noreply.github.com>
2025-08-19 18:53:50 +0200

    format nix flake

--- flake.nix
+++ flake.nix
@@ -46,18 +46,20 @@
         };
         devShells.default = let
           hpkgs = pkgs.haskell.packages.ghc98;
-          tools = with hpkgs; [
-            cabal-fmt
-            cabal-install
-            fourmolu
-            ghc
-            ghc-prof-flamegraph
-            ghcid
-            profiteur
-          ] ++ (with pkgs; [
-            haskell-language-server
-            nixd
-          ]);
+          tools = with hpkgs;
+            [
+              cabal-fmt
+              cabal-install
+              fourmolu
+              ghc
+              ghc-prof-flamegraph
+              ghcid
+              profiteur
+            ]
+            ++ (with pkgs; [
+              haskell-language-server
+              nixd
+            ]);
           libraries = [
             pkgs.zlib
           ];
